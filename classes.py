aaf3229624fdc644b98ddf51492f62cbad865e97
pnw <patrick@levr.com>
2012-12-02 17:31:48 -0500

    removed geo_hash from node. unnecessary

--- classes.py
+++ classes.py
@@ -10,27 +10,23 @@ class GHash(ndb.Model):
 
 class Node(ndb.Model):
 	# position of this node in the way
-	idx = ndb.IntegerProperty(required=True)
+	idx = ndb.IntegerProperty()
 	# the node's position
 	geo_point = ndb.GeoPtProperty(required=True, indexed=False)
-	# the node's geohash
-	geo_hash = ndb.ComputedProperty(
-			lambda self: geohash.encode(
-				latitude = self.geo_point.lat,
-				longitude = self.geo_point.lon,
-				precision = 8
-				)
-			)
+#	# the node's geohash
+#	geo_hash = ndb.ComputedProperty(
+#			lambda self: geohash.encode(
+#				latitude = self.geo_point.lat,
+#				longitude = self.geo_point.lon,
+#				precision = 8
+#				)
+#			)
 
-class Road(ndb.Model):
-	# the actual nodes
+class Shape(polymodel.PolyModel):
+	# has a shape definition from open maps
 	nodes = ndb.StructuredProperty(Node,repeated=True)
-
-class Building(ndb.Model):
-	nodes = ndb.StructuredProperty(Node,repeated=True)
-	# the building does not have a shape definition from open maps
+	# does not have a shape definition from open maps
 	geo_point = ndb.GeoPtProperty(indexed=False)
-	# the node's geohash
 	geo_hash = ndb.ComputedProperty(
 			lambda self: geohash.encode(
 				latitude = self.geo_point.lat,
@@ -38,16 +34,11 @@ class Building(ndb.Model):
 				precision = 9
 				)
 			)
-class Nature(ndb.Model):
-	# the building has a shape definition from open maps
-	nodes = ndb.StructuredProperty(Node,repeated=True)
-	# the building does not have a shape definition from open maps
-	geo_point = ndb.GeoPtProperty(indexed=False)
-	# the node's geohash
-	geo_hash = ndb.ComputedProperty(
-			lambda self: geohash.encode(
-				latitude = self.geo_point.lat,
-				longitude = self.geo_point.lon,
-				precision = 9
-				)
-			)
+	
+class Road(Shape):
+	pass
+
+class Building(Shape):
+	pass
+class Nature(Shape):
+	pass
--- utils.py
+++ utils.py
@@ -3,8 +3,29 @@ import webapp2
 
 
 class BaseHandler(webapp2.RequestHandler):
-	pass
+	def say(self,stuff):
+		self.response.out.write(stuff)
 
-class Populate(object):
+class PopulateGHash(object):
+	def __init__(self,ghash):
+		assert type(ghash) == str, type(ghash)
+		assert ghash.__len__() == 6, ghash.__len__()
+		self.ghash = ghash
+	def create_ghash_entity(self,
+						roads=None,
+						buildings=None,
+						nature=None,
+						**kwargs):
+		ghash_entity = classes.GHash(id=self.ghash)
+		if roads:
+			ghash_entity.roads = roads
+		if buildings:
+			ghash_entity.buildings = buildings
+		if nature:
+			ghash_entity.nature = nature
+		
+		if kwargs.get('auto_put',True) == True:
+			ghash_entity.put()
+		return ghash_entity
 	
-	def geo_hash(self,roads,buildings,nature):
+	def create_nature_object(self):
