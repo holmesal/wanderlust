ee83f2109d1fdf90b56af0701a9cd01aa483a8f3
Alonso Holmes <holmesal@bu.edu>
2012-12-02 22:28:29 -0500

    fix

--- gplaces.py
+++ gplaces.py
@@ -11,7 +11,7 @@ class Gplaces(object):
 	def __init__(self,ghash_entity):
 		geo_point = self.ghash_entity.geo_point
 		
-		self.base_url = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?'
+		self.base_url = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?key=AIzaSyCjddKUEHrVcCDqA9fqLMPUBXH0mrWPpgI'
 		
 	def get_data(self):
-		
+		pass
